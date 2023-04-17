1b22434cd2cd444698c2fa5005326ef3cbb644f6
JRSeabaugh1s <JRSeabaugh1s@capetigers.com>
2023-04-17 09:07:37 -0500

    first commit

---
+++ ag_poker.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
+   Tutorial 14
+   Tutorial Case
+
+   Author: Jaedan
+   Date:  4/17/23 
+   
+   Filename: ag_poker.js
+
+*/
+
